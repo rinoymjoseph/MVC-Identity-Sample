267085c3846e88bde884b40be3783a33e2454ffb
Rinoy Joseph <rinoymjoseph@gmail.com>
2017-01-05 18:26:29 +0530

    pushing files

---
+++ WebAppFormAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppFormAuth.Startup))]
+namespace WebAppFormAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
